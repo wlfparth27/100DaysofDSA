099b867a739b5d86a647e857fcbd2c4a1891583e
Parth <wlfparth27@gmail.com>
2026-02-16 15:04:55 +0000

    Day 2

---
+++ question2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, pos, x;
+    scanf("%d", &n);
+
+    int arr[n + 1];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &pos);
+    scanf("%d", &x);
+
+    for (int i = n; i >= pos; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[pos - 1] = x;
+
+    for (int i = 0; i <= n; i++)
+    {
+        printf("%d%s", arr[i], (i == n) ? "" : " ");
+    }
+
+    return 0;
+}
